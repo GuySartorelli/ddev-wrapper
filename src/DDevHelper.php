0af66b1f25a23ebbd35eec2592ca1d17301c410e
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-12-11 22:17:58 +1300

    Allow running DDEV commands interactively only on verbose
    Also fix bug with JSON return type and update PHPdocs

--- src/DDevHelper.php
+++ src/DDevHelper.php
@@ -3,12 +3,15 @@
 namespace GuySartorelli\DdevWrapper;
 
 use stdClass;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
 class DDevHelper
 {
     /**
      * Run a DDEV command interactively (assumes TTY is supported)
+     *
+     * @return bool Whether the command was successful or not
      */
     public static function runInteractive(string $command, array $args = []): bool
     {
@@ -20,7 +23,26 @@ class DDevHelper
     }
 
     /**
+     * Run a DDEV command interactively (assumes TTY is supported) when output is verbose - but otherwise run it normally.
+     * Takes an optional callback to handle output when not running interactively.
+     *
+     * @return bool Whether the command was successful or not
+     */
+    public static function runInteractiveOnVerbose(string $command, array $args = [], OutputInterface $output, ?callable $quietModeCallback = null): bool
+    {
+        if ($output->isVerbose()) {
+            return static::runInteractive($command, $args);
+        }
+
+        $process = new Process(['ddev', $command, ...$args]);
+        $process->run($quietModeCallback);
+        return $process->isSuccessful();
+    }
+
+    /**
      * Run a DDEV command non-interactively
+     *
+     * @return string The output from the command
      */
     public static function run(string $command, array $args = []): string
     {
@@ -31,8 +53,10 @@ class DDevHelper
 
     /**
      * Run a DDEV command and get the output as JSON
+     *
+     * @return mixed The output from the command as JSON output (e.g. stdClass), or null if there was no JSON output.
      */
-    public static function runJson(string $command, array $args = []): ?stdClass
+    public static function runJson(string $command, array $args = []): mixed
     {
         $response = json_decode(static::run($command, [...$args, '--json-output']), false);
         return $response?->raw ?? null;
@@ -42,6 +66,7 @@ class DDevHelper
      * Get the details of the project, if it exists.
      *
      * @param string $project The name of the project to get details for. If ommitted, the CWD is used.
+     * @return stdClass|null The output from the command as a JSON object, or null if there was no JSON output.
      */
     public static function getProjectDetails(string $project = ''): ?stdClass
     {
