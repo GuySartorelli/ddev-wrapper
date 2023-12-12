3765b11ffb5bcfdd76799403d76f3bbe5b398e54
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-12-12 23:00:35 +1300

    Pass through completion requests to DDEV for arguments
    This gives correct results the first completion request, but subsequent
    requests give the same result again, so you can get e.g:
    ddev auth ssh
    but then you press tab again and get
    ddev auth ssh ssh
    
    At this point I'm favouring having it correctly get me the first
    autocompletion - subcommands are fairly rare anyway so I probably won't
    use it much either way.

--- src/Command/PassThroughCommand.php
+++ src/Command/PassThroughCommand.php
@@ -5,6 +5,7 @@ namespace GuySartorelli\DdevWrapper\Command;
 use GuySartorelli\DdevWrapper\DDevHelper;
 use RuntimeException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Completion\CompletionInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
@@ -185,7 +186,19 @@ class PassThroughCommand extends Command
         }
 
         // We can't easily detect which commands should allow arguments, so lets just allow an array of arguments.
-        $this->addArgument('arguments', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Arguments to be passed through to the DDEV command, if any. See help below.');
+        $this->addArgument(
+            'arguments',
+            InputArgument::IS_ARRAY | InputArgument::OPTIONAL,
+            'Arguments to be passed through to the DDEV command, if any. See help below.',
+            null,
+            // Pass completion request through to ddev in case there are sub-commands or valid arguments.
+            // Note that because this is an array type (to account for possible multi-nested commands), it will
+            // give the same value multiple times (e.g. ddev auth \t ssh \t ssh)
+            function (CompletionInput $input) {
+                $ddevCompletion = DDevHelper::run('__complete', [$this->getName(), $input->getCompletionValue()]);
+                return array_filter(explode("\n", $ddevCompletion), fn ($option) => !str_starts_with($option, ':'));
+            }
+        );
 
         $this->initialised = true;
     }
