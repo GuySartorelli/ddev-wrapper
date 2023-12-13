0c3e71302c56c85e8134e42ececa61541c9f99a6
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-12-13 16:52:36 +1300

    Remove unused "use" statement

--- src/Application.php
+++ src/Application.php
@@ -11,7 +11,6 @@ use Symfony\Component\Console\Command\HelpCommand as BaseHelpCommand;
 use Symfony\Component\Console\Command\ListCommand;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends BaseApplication
